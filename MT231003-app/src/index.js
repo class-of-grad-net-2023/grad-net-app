783eb7ef4ab0e977d5c4b01ce2531c095ff65a33
1000v00 <mt23a003@oecu.jp>
2023-12-20 12:13:44 +0900

    Added function pull_todo_elements

--- MT231003-app/src/index.js
+++ MT231003-app/src/index.js
@@ -1,5 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import Hello from "./components/Hello";
+import todolist from "./../data/todos.json";
+
+function pull_todos_element( {todos = [] } )
+{
+    if(!todos.length) return <div>There is nothing to do now.</div>;
+    return (
+        todos.map(<p>userId : { userId }, Id : { Id }, title : { title }, completed : { completed }</p>)
+    );
+}
+
 
 ReactDOM.render(<Hello />, document.getElementById("root"));
