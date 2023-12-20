8c3e175f94f52fd97c1400e9f4eab2a754b43d48
1000v00 <mt23a003@oecu.jp>
2023-12-20 23:19:21 +0900

    Implemented delete button and feature to delete ToDo.

--- MT231003-app/src/components/ToDoList.js
+++ MT231003-app/src/components/ToDoList.js
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
 import Format_todo from "./Format_todo_item.js";
 
-export default function Pull_todos_elements( {todos = [] } )
+export default function ToDolist( {todos = [], onRemoveTodo = f => f } )
 {
     if(!todos.length) return <div>There is nothing to do now.</div>;
     return (
         <div>
             <h1>ToDoList</h1>
             {
-                todos.map(todo => <Format_todo key={todo.id} {...todo} />)
+                todos.map(todo => <Format_todo key={todo.id} {...todo} onRemove={onRemoveTodo} />)
             }
         </div>
     );
