55ce94e7082da27a84d6d7a04334f869a4410aaa
1000v00 <mt23a003@oecu.jp>
2023-12-21 15:52:24 +0900

    Implemented feature to make it easier to see, such as lines per line and lines for completed tasks.

--- MT231003-app/src/components/Format_todo_item.js
+++ MT231003-app/src/components/Format_todo_item.js
@@ -4,6 +4,7 @@ import { FaTrash } from "react-icons/fa";
 export default function Format_todo({ id, userId, title, completed, onRemove = f => f , onChangeCompleted = f => f})
 {
     const done = completed ? "O" : "X";
+    const format_title = title.length < 60 ? title : title.slice(0, 60) + "...";
     const number = id.toString().padStart(3, '0');
     return (
         <>
@@ -12,9 +13,13 @@ export default function Format_todo({ id, userId, title, completed, onRemove = f
                     <FaTrash />
                 </button>
                 &emsp;
-                No.{ number } - userID : { userId },  done??? : <button onClick={() => onChangeCompleted(id)}>{ done }</button>,  title : { title }
-
+                No.{ number } - userID : { userId }
+                &emsp;
+                done??? : <button onClick={() => onChangeCompleted(id)}>{ done }</button>
+                &emsp;
+                title : { completed ? <s>{format_title}</s> : format_title }
             </p>
+            <hr style={{color: "#AFAEB3", width: "100%"}} />
         </>
     );
 }
