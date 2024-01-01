afc10b978b44266e28730891f90428da50025995
pixel-o8 <mt23a001@MT23A001>
2024-01-02 00:55:48 +0900

    ロゴを表示

---
+++ MT231001-app/src/components/header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../css/style.css";
+
+const Header_todo = () => {
+  return (
+    <div className="header">
+      <h1 className="header_logo">TODO Application!</h1>
+    </div>
+  );
+};
+
+export default Header_todo;
