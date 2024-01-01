24a0c8ecac4c133f9f328369531ae9f6725d1bda
pixel-o8 <mt23a001@MT23A001>
2024-01-02 00:56:36 +0900

    ナビゲーションを追加
    ナビゲーションを追加

--- MT231001-app/src/components/header.js
+++ MT231001-app/src/components/header.js
@@ -5,6 +5,28 @@ const Header_todo = () => {
   return (
     <div className="header">
       <h1 className="header_logo">TODO Application!</h1>
+      <nav className="header_nav">
+        <ul className="header_list">
+          <li className="header_item">
+            <a href="">私たちの特徴</a>
+          </li>
+          <li className="header_item">
+            <a href="">サービス</a>
+          </li>
+          <li className="header_item">
+            <a href="">実績</a>
+          </li>
+          <li className="header_item">
+            <a href="">ブログ</a>
+          </li>
+          <li className="header_item">
+            <a href="">会社概要</a>
+          </li>
+          <li className="header_item_contact">
+            <a href="">お問い合わせ</a>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
