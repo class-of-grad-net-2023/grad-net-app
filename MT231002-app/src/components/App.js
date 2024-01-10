acee54f50b33bab3b3892605aa80f72e81458b97
mt23a002 <mt23a002@oecu.jp>
2024-01-10 03:16:51 +0000

    2024 01 10 12:17

--- MT231002-app/src/components/App.js
+++ MT231002-app/src/components/App.js
@@ -4,7 +4,12 @@ import  AlbumsList from "./Albums/AlbumsList.js"
 
 export default function App() {
     const [albums]=useState(albumsData);
+    const albums2=[];
+    console.log(albums2);
+    for(let i=0;i<5;i++){
+        albums2.push(albums[i]);
+    }
     return (
-    <AlbumsList albums={albums}/>
+    <AlbumsList albums={albums2}/>
     );
 }
