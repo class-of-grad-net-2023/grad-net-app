bede901239755a11e55ea3302c765b64c1f48765
mt23a002 <mt23a002@oecu.jp>
2024-01-10 03:47:01 +0000

    2024 01 10 12:47

--- MT231002-app/src/components/App.js
+++ MT231002-app/src/components/App.js
@@ -6,9 +6,12 @@ export default function App() {
     const [albums]=useState(albumsData);
     const albums2=[];
     console.log(albums2);
-    for(let i=0;i<5;i++){
+
+    //データ分割
+    for(let i=0;i<10;i++){
         albums2.push(albums[i]);
     }
+    
     return (
     <AlbumsList albums={albums2}/>
     );
