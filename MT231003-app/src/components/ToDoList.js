a2b7e48536f27196674080ac757769ae2eda1c08
1000v00 <mt23a003@oecu.jp>
2024-01-10 12:46:25 +0900

    Implemented a few background colors and movements.

--- MT231003-app/src/components/ToDoList.js
+++ MT231003-app/src/components/ToDoList.js
@@ -14,20 +14,24 @@ export default function ToDolist({
     const page_amount = 10;
     if(!todos.length) return <div>There is nothing to do now.</div>;
     return (
-        <div style={{ backgroundColor: "white", padding: "0px" }}>
-            {
-                todos.map(todo => 
-                            <Format_todo 
-                                key={todo.id}
-                                {...todo}
-                                onRemove={onRemoveTodo}
-                                onChangeCompleted={onChangeCompleted} 
-                            />
-                        ).slice(page*page_amount, page*page_amount+page_amount)
-            }
-            <p>&ensp;{page+1} / {Math.ceil((todos.length/page_amount))}&ensp;</p>
-            { todos.length/page_amount > page+1 && <button onClick={onNextPage}> next </button> }
-            { 0 !== page && <button onClick={onPreviousPage}> previous </button> }
+        <div>
+            <div style={{ backgroundColor: "white", padding: "0px" }}>
+                {
+                    todos.map(todo => 
+                                <Format_todo 
+                                    key={todo.id}
+                                    {...todo}
+                                    onRemove={onRemoveTodo}
+                                    onChangeCompleted={onChangeCompleted} 
+                                />
+                            ).slice(page*page_amount, page*page_amount+page_amount)
+                }
+            </div>
+            <div style={{ textAlign: "center" }}>
+                <p style={{ backgroundColor: "darkkhaki", padding: "10px 0px" }}>&ensp;{page+1} / {Math.ceil((todos.length/page_amount))}&ensp;</p>
+                { todos.length/page_amount > page+1 && <span onClick={onNextPage}> <u>next</u> </span> }
+                { 0 !== page && <span onClick={onPreviousPage}> <u>previous</u> </span> }
+            </div>
         </div>
     );
 }
