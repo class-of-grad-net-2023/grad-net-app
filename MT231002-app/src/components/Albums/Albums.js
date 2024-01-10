9dab4ba707b65a2275fd8331f379afb35a1ad0d9
mt23a002 <mt23a002@oecu.jp>
2024-01-10 09:14:56 +0000

    2024 01 10 18:15

--- MT231002-app/src/components/Albums/Albums.js
+++ MT231002-app/src/components/Albums/Albums.js
@@ -2,8 +2,10 @@ import React from "react";
 export default function Albums({userId,id,title}){
     return(
         <div>
-            {"userID:"}{userId}{", id: "}{id}{", title: "}{title}
-            <button class="favorite styled" type="button">削除</button>
+            <li>
+                {"userID:"}{userId}{", id: "}{id}{", title: ”"}{title}{"” "}
+                <button className="delete">削除</button>
+            </li>
         </div>
     );
 }
--- MT231002-app/src/components/App.js
+++ MT231002-app/src/components/App.js
@@ -5,9 +5,10 @@ import  AlbumsList from "./Albums/AlbumsList.js"
 export default function App() {
     const [albums]=useState(albumsData);
     const albums2=[];
+    var page=0;
 
     //データ分割
-    for(let i=0;i<10;i++){
+    for(let i=page;i<10;i++){
         albums2.push(albums[i]);
     }
 
@@ -15,8 +16,9 @@ export default function App() {
     <div>
         <h1>{"アルバム"}</h1>
         <AlbumsList albums={albums2}/>
-        <button class="favorite styled" type="button">前へ</button>
-        <button class="favorite styled" type="button">次へ</button>
+        <button className="before">前へ</button>
+        {" "}
+        <button className="after">次へ</button>
     </div>
     );
 }
