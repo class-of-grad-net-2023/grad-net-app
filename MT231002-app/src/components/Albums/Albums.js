2e353b2b8f95656161b04a8adafa6a9ff68f6f5a
mt23a002 <mt23a002@oecu.jp>
2024-01-10 07:11:54 +0000

    2024 01 10 16:12

--- MT231002-app/src/components/Albums/Albums.js
+++ MT231002-app/src/components/Albums/Albums.js
@@ -3,6 +3,7 @@ export default function Albums({userId,id,title}){
     return(
         <div>
             {"userID:"}{userId}{", id: "}{id}{", title: "}{title}
+            <button class="favorite styled" type="button">削除</button>
         </div>
     );
 }
