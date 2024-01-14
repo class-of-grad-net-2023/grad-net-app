60ebe5e53f2ad32cb18c8e9a83f0a46305a3e6a2
mt23a002 <mt23a002@oecu.jp>
2024-01-14 03:54:48 +0000

    2024 01 14 12:55

--- MT231002-app/src/components/App.js
+++ MT231002-app/src/components/App.js
@@ -3,34 +3,48 @@ import albumsData from "../../data/albums.json"
 import  AlbumsList from "./Albums/AlbumsList.js"
 
 export default function App() {
-    const [albums]=useState(albumsData);
-    const albums2=[];
-    var page=0;
+    const [albums,setAlbums]=useState(albumsData);
+    const [page, setPage]=useState(0);
+    const [albums2, setAlbums2]=useState(albums.slice(page,10));
 
-    //データ分割
-    for(let i=page;i<10;i++){
-        if(albums[i]!==null){
-            albums2.push(albums[i]);
-        }
+    
+    function forwardPage(){
+        setPage(page+10);
+        setAlbums2(albums.slice(page,10));
     }
+    
+    function backPage(){
+        setPage(page-10);
+        setAlbums2(albums.slice(page,10));
+    }
+    
 
     if(page<1){
         return (
         <div>
             <h1>{"アルバム"}</h1>
             <AlbumsList albums={albums2}/>
-            <button className="after">次へ</button>
+            <button className="after" onClick={()=>forwardPage()}>次へ</button>
         </div>
+        );
+    
+    }else if(page+10>albums.length){
+        return (
+            <div>
+                <h1>{"アルバム"}</h1>
+                <AlbumsList albums={albums2}/>
+                <button className="before"onClick={()=>backPage()}>前へ</button>
+            </div>
         );
     }else{
         return (
             <div>
                 <h1>{"アルバム"}</h1>
                 <AlbumsList albums={albums2}/>
-                <button className="before">前へ</button>
+                <button className="before"onClick={()=>backPage()}>前へ</button>
                 {" "}
-                <button className="after">次へ</button>
+                <button className="after" onClick={()=>forwardPage()}>次へ</button>
             </div>
-            );
+        );
     }
 }
---
+++ MT231002-app/src/components/Modules/delete.js
@@ -0,0 +1,3 @@
+export default function deleteData(box=[],i){
+    return box.splice(i,1);
+}
