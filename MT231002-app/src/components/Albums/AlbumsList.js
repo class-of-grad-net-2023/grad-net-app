e11d3ebf11c9b9daba1bc477f2304211fd0c51aa
mt23a002 <mt23a002@oecu.jp>
2024-01-18 15:17:32 +0000

    delete機能実装完了 2024 01 19 0:17

--- MT231002-app/src/components/Albums/AlbumsList.js
+++ MT231002-app/src/components/Albums/AlbumsList.js
@@ -1,12 +1,12 @@
 import React, { useId } from "react";
 import Albums from "./Albums.js";
 
-export default function AlbumsList({albums=[]}){
+export default function AlbumsList({albums=[],onRemuveAlbums=f=>f}){
     if(!albums.length) return <div>No Albums Listed.</div>
     return(
         <div>
             {
-                albums.map(albums=><Albums key={albums.id} {...albums}/>)
+                albums.map(albums=><Albums key={albums.id} {...albums} onRemove={onRemuveAlbums}/>)
             }
         </div>
     );
--- MT231002-app/src/components/App.js
+++ MT231002-app/src/components/App.js
@@ -1,19 +1,20 @@
 import React, {useState} from "react";
 import albumsData from "../../data/albums.json"
 import  AlbumsList from "./Albums/AlbumsList.js"
+import Albums from "./Albums/Albums.js";
 
 export default function App() {
-    const [albums]=useState(albumsData);
+    const [albums,setAlbums]=useState(albumsData);
     const [page, setPage]=useState(0);
     const [albums2, setAlbums2]=useState(albums.slice(page,10));
 
     
-    function forwardPage(){
+    const forwardPage=()=>{
         setPage(page+10);
         setAlbums2(albums.slice(page,10));
     }
     
-    function backPage(){
+    const backPage=()=>{
         setPage(page-10);
         setAlbums2(albums.slice(page,10));
     }
@@ -23,27 +24,48 @@ export default function App() {
         return (
         <div>
             <h1>{"アルバム"}</h1>
-            <AlbumsList albums={albums2}/>
-            <button className="after" onClick={()=>forwardPage()}>次へ</button>
+            <AlbumsList
+             albums={albums2}
+             onRemuveAlbums={id=>{
+                const newAlbums = albums.filter(albums=>albums.id!==id);
+                setAlbums(newAlbums);
+                setAlbums2(albums.slice(page,10));
+             }}
+            />
+            <button className="after" onClick={forwardPage}>次へ</button>
         </div>
         );
     
     }else if(page+10>albums.length){
         return (
             <div>
-                <h1>{"アルバム"}</h1>
-                <AlbumsList albums={albums2}/>
-                <button className="before"onClick={()=>backPage()}>前へ</button>
+            <h1>{"アルバム"}</h1>
+            <AlbumsList
+             albums={albums2}
+             onRemuveAlbums={id=>{
+                const newAlbums = albums.filter(albums=>albums.id!==id);
+                setAlbums(newAlbums);
+                setAlbums2(albums.slice(page,10));
+             }}
+            />
+                <button className="before"onClick={backPage}>前へ</button>
             </div>
         );
     }else{
         return (
             <div>
-                <h1>{"アルバム"}</h1>
-                <AlbumsList albums={albums2}/>
-                <button className="before"onClick={()=>backPage()}>前へ</button>
+            <h1>{"アルバム"}</h1>
+            <AlbumsList
+             albums={albums2}
+             onRemuveAlbums={id=>{
+                const newAlbums = albums.filter(albums=>albums.id!==id);
+                setAlbums(newAlbums);
+                setAlbums2(albums.slice(page,10));
+             }}
+            />
+                <button className="before"onClick={backPage}>前へ</button>
                 {" "}
-                <button className="after" onClick={()=>forwardPage()}>次へ</button>
+                <button className="after" onClick={forwardPage}>次へ</button>
             </div>
         );
     }
