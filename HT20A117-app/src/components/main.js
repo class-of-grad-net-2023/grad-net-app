cea92cc852c5290c35481f98bde2cc1dc86608f3
KaitoWatanabe <ht20a117@oecu.jp>
2024-01-27 01:28:40 +0900

    ページャーを追加

--- HT20A117-app/src/components/main.js
+++ HT20A117-app/src/components/main.js
@@ -1,30 +1,38 @@
 // main.js
 import React, { useState, useEffect }from "react";
 import Data from "../../data//albums.json";
+import Nexting from "./Next";
 import "../css/design.css";
 
 const Main = () => {
   const [AlbumArray, setAlbum] = useState([]);
+  const [Page, NewPage] = useState(1);
 
   useEffect(() => {
     setAlbum(Data);
   }, []);
-  
-  const AlbumItems = AlbumArray.slice(1, 10);
+
+  const number = 10;
+  const Last = Page * 10;
+  const First = Last - 10;
+  const AlbumItems = AlbumArray.slice(First, Last);
+
 
   const Delete = id => {
     const New = AlbumArray.filter((item) => item.id !== id);
     setAlbum(New);
   }
 
+  const Next = (Nextpage) => NewPage(Nextpage);
+
   return (
     <div>
-      <div className="main-content-warpper">
+      <div className="main">
           <div className="title">Album</div>
         <ul>
           {AlbumItems.map((album, index) => (
             <li key={index}>
-              <ul className="list-container">
+              <ul className="album">
                 <li>UserId: {album.userId},ID: {album.id},Title: {album.title}
                 <button className="delete-button"
                       onClick={() => Delete(album.id)}>
@@ -36,6 +44,9 @@ const Main = () => {
           ))}
         </ul>
       </div>
+      <Nexting
+        Albumnumber={number} Total={AlbumArray.length} Page={Page} Nexts={Next}
+        />
     </div>
   );
 };
